b4ce4851e0ac19c8e803c0c11ba7193d14208813
seungmin-lee-dotcom <k07217@naver.com>
2023-01-11 15:31:27 +0900

    refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import "../styles/globals.css";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { Button } from "react-bootstrap";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="componentst">
+    <div className="wholecomponents">
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,3 @@
-import { Button } from "react-bootstrap";
-
 export default function About() {
   return (
     <>
